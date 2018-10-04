f4bdc69bfb173888350cd3419009b5ea8609ed57
Meagan Chase <meaganchase814@yahoo.com>
2018-10-03 20:04:08 -0400

    simple set up

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -1,4 +1,13 @@
 class PollController < ApplicationController
   def index
+    @poll = Option.all
+  end
+  def create
+    @poll = Posit.new
+    @poll.name = params["name"] 
+    @poll.tally = params["tally"] 
+    
+    @poll.save
+    redirect_to poll_index_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'poll/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'poll/create'
+  root 'poll#index'
 end
